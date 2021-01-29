0e6689099db28cf863246de572a4c705e1d5a4ee
Paulo Sérgio <ps131296@gmail.com>
2021-01-29 11:37:52 -0300

    refactored HTTPClientSpy to store requests url and params in a single array, removing duplicated test

--- RegisterTests/RemoteUserRegister.test.ts
+++ RegisterTests/RemoteUserRegister.test.ts
@@ -17,17 +17,7 @@ describe('RemoteUserRegister', () => {
     const client = new HTTPClientSpy()
     new RemoteUserRegister(client)
 
-    expect(client.requestedURLs).toEqual([])
-  });
-
-  test('register requests data from url', () => {
-    const url = new URL("http://any-url.com")
-    const client = new HTTPClientSpy()
-    const sut = new RemoteUserRegister(client)
-
-    sut.register(url, {})
-
-    expect(client.requestedURLs).toEqual([url])
+    expect(client.requests).toEqual([])
   });
 
   test('register requests data from url with correct params', () => {
@@ -44,16 +34,14 @@ describe('RemoteUserRegister', () => {
 
     sut.register(url, params)
 
-    expect(client.requestedParams).toEqual([params])
+    expect(client.requests[0]).toEqual({ url, params})
   });
 
   class HTTPClientSpy implements HTTPClient<UserRegisterModel> {
-    requestedURLs: URL[] = []
-    requestedParams: UserRegisterModel[] = []
+    requests: { url: URL, params: UserRegisterModel }[] = []
 
     get(url: URL, params: UserRegisterModel): void {
-      this.requestedURLs.push(url)
-      this.requestedParams.push(params)
+      this.requests.push({ url, params })
     }
   }
 })
