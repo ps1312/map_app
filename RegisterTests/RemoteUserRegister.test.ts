fea4997dd4c3191878c027e01b73acf0205ac911
Paulo Sérgio <ps131296@gmail.com>
2021-01-29 11:45:19 -0300

    moded sut, client, url and HTTPClient params init to helper functions

--- RegisterTests/RemoteUserRegister.test.ts
+++ RegisterTests/RemoteUserRegister.test.ts
@@ -14,28 +14,37 @@ class RemoteUserRegister {
 
 describe('RemoteUserRegister', () => {
   test('init does not request data from url', () => {
-    const client = new HTTPClientSpy()
-    new RemoteUserRegister(client)
+    const { client } = makeSUT()
 
     expect(client.requests).toEqual([])
   });
 
   test('register requests data from url with correct params', () => {
-    const url = new URL("http://any-url.com")
-    const client = new HTTPClientSpy()
-    const sut = new RemoteUserRegister(client)
+    let url = anyURL()
+    const { sut, client } = makeSUT()
+    let params = anyUserRegisterModel()
   
-    const params: UserRegisterModel = {
-      username: 'any-username',
-      email: 'any-email@mail.com',
-      password: 'any-password',
-      passwordConfirmation: 'any-password',
-    }
-
     sut.register(url, params)
 
-    expect(client.requests[0]).toEqual({ url, params})
+    expect(client.requests[0]).toEqual({ url, params })
   });
+
+  type SutTypes = { sut: RemoteUserRegister, client: HTTPClientSpy }
+  function makeSUT(): SutTypes {
+    const client = new HTTPClientSpy()
+    const sut = new RemoteUserRegister(client)
+
+    return { sut, client }
+  }
+
+  function anyURL(): URL {
+    return new URL("http://any-url.com");
+  }
+
+  function anyUserRegisterModel(): UserRegisterModel {
+    return { username: 'any-username', email: 'any-email@mail.com',
+            password: 'any-password', passwordConfirmation: 'any-password' }
+  }
 
   class HTTPClientSpy implements HTTPClient<UserRegisterModel> {
     requests: { url: URL, params: UserRegisterModel }[] = []
