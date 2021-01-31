b4a104324b403e0dec5824ffd4dcd4a04a506be8
Paulo Sérgio <ps131296@gmail.com>
2021-01-31 14:47:36 -0300

    extracted sut and client spy instantiation to a helper function

--- src/tests/unit/LoginUserUseCase.unit.test.ts
+++ src/tests/unit/LoginUserUseCase.unit.test.ts
@@ -16,22 +16,27 @@ class RemoteUserLogin {
 
 describe('RemoteUserLogin', () => {
   test('init does not make requests', () => {
-    const client = new HTTPClientSpy()
-    new RemoteUserLogin(anyURL(), client)
+    const [, client] = makeSUT()
 
     expect(client.requests).toStrictEqual([])
   })
 
   test('login requests data from url with correct params', async () => {
-    const url = anyURL()
-    const client = new HTTPClientSpy()
-    const sut = new RemoteUserLogin(url, client)
+    const url = new URL("http://another-url.com")
+    const [sut, client] = makeSUT(url)
     const params = anyUserLoginModel()
 
     await sut.login(params)
 
     expect(client.requests[0]).toEqual({ url, params })
   })
+
+  function makeSUT(url: URL = anyURL()): [RemoteUserLogin, HTTPClientSpy] {
+    const client = new HTTPClientSpy()
+    const sut = new RemoteUserLogin(url, client)
+
+    return [sut, client]
+  }
 
   function anyUserLoginModel() {
     return { email: "eve.holt@reqres.in", password: "cityslicka" }
