b51ac130bc6b7d491be0ffd5f0c95724117fa559
Paulo Sérgio <ps131296@gmail.com>
2021-02-01 20:33:43 -0300

    delivers null result on empty local store

--- src/tests/unit/UserLocalStore.unit.test.ts
+++ src/tests/unit/UserLocalStore.unit.test.ts
@@ -1,4 +1,12 @@
-class UserLocalStore {}
+class UserLocalStore {
+  constructor(
+    private readonly store: Storage,
+  ) {}
+
+  retrieve() {
+    return this.store.getItem("user")
+  }
+}
 
 describe('UserLocalStore', () => {
   afterEach(() => {
@@ -6,8 +14,14 @@ describe('UserLocalStore', () => {
   })
 
   test('init does not have any side effect', () => {
-    new UserLocalStore()
+    new UserLocalStore(localStorage)
     expect(localStorage.length).toEqual(0)
+  })
+
+  test('delivers null result on empty local store', () => {
+    const sut = new UserLocalStore(localStorage)
+    const result = sut.retrieve()
+    expect(result).toStrictEqual(null)
   })
 })
 
