aa27deb1faef0aa209b3ea3ffdfa3e056a82a214
Paulo Sérgio <ps131296@gmail.com>
2021-02-01 14:04:35 -0300

    RemoteGetUser delivers valid User object on 200 status code and a valid JSON body

--- src/tests/unit/GetProfileUseCase.unit.test.ts
+++ src/tests/unit/GetProfileUseCase.unit.test.ts
@@ -3,22 +3,49 @@ import { HTTPClient, HTTPClientResponse } from "../../services/http/HTTPClient"
 import { HTTPClientSpy } from "./Helpers/HTTPClientSpy"
 import { anyURL } from "./Helpers/SharedHelpers"
 
+type User = {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
+
 class RemoteGetProfile {
   constructor(
     private readonly url: URL,
     private readonly client: HTTPClient,
   ) {}
 
-  async load(userId: number): Promise<void> {
+  async load(userId: number): Promise<User> {
     const userURL = new URL(`${userId}`, this.url)
     const result = await this.client.get(userURL)
 
     if (result instanceof HTTPClientResponse) {
+      const { statusCode, body } = result
+
+      if (statusCode === 200 && isResult(body)) {
+        return body.data
+      }
       throw new InvalidDataError()
     }
 
     throw new NoConnectivityError()
   }
+}
+
+type GetUserResultBody = {
+  data: {
+    id: number;
+    email: string;
+    first_name: string;
+    last_name: string;
+    avatar: string;
+  }
+}
+
+function isResult(result: GetUserResultBody): result is GetUserResultBody {
+  return (result as GetUserResultBody).data !== undefined;
 }
 
 describe('RemoteGetProfile', () => {
@@ -68,6 +95,25 @@ describe('RemoteGetProfile', () => {
 
     client.completeWithSuccess(200, {})
     await expect(sut.load(anyUserId())).rejects.toEqual(new InvalidDataError())
+  })
+
+  test('delivers requested user data on 200 status code and valid JSON body', async () => {
+    const [sut, client] = makeSUT()
+
+    const validBody = {
+      "data": {
+          "id": 2,
+          "email": "janet.weaver@reqres.in",
+          "first_name": "Janet",
+          "last_name": "Weaver",
+          "avatar": "https://reqres.in/img/faces/2-image.jpg"
+      },
+    }
+
+    client.completeWithSuccess(200, validBody)
+    const result = await sut.load(anyUserId())
+    
+    expect(result).toStrictEqual(validBody['data'])
   })
 
   function makeSUT(url: URL = anyURL()): [RemoteGetProfile, HTTPClientSpy] {
