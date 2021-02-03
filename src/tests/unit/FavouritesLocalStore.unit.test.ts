c258cbf1f7bd7c3be484fcd20913c85c3edd908d
Paulo Sérgio <ps131296@gmail.com>
2021-02-03 16:29:53 -0300

    retrieve array of inserted favourites places in local storage

--- src/tests/unit/FavouritesLocalStore.unit.test.ts
+++ src/tests/unit/FavouritesLocalStore.unit.test.ts
@@ -1,4 +1,16 @@
-class FavouritesLocalStore {}
+class FavouritesLocalStore {
+  static localStorageKey = "favourites"
+
+  insert(placeId: string) {
+    localStorage.setItem(FavouritesLocalStore.localStorageKey, JSON.stringify([placeId]))
+  }
+
+  retrieve() {
+    const string = localStorage.getItem(FavouritesLocalStore.localStorageKey)
+    const favourites = JSON.parse(string as string)
+    return favourites
+  }
+}
 
 describe('FavouritesLocalStore', () => {
   afterEach(() => {
@@ -8,6 +20,12 @@ describe('FavouritesLocalStore', () => {
   test('init does not have side effects', () => {
     new FavouritesLocalStore()
     expect(localStorage.length).toEqual(0)
+  })
+
+  test('retrieve array of inserted favourites places in local storage', () => {
+    const sut = new FavouritesLocalStore()
+    sut.insert("any-place-id")
+    expect(sut.retrieve()).toHaveLength(1)
   })
 })
 
